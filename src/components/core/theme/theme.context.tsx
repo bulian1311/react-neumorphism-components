91781258f06a2bb8b7551a5422a98b22f325d6c5
bulian1311 <bulian1311@gmail.com>
2021-12-28 12:14:45 +0300

    Theme context.

--- src/components/core/theme/theme.context.tsx
+++ src/components/core/theme/theme.context.tsx
@@ -21,6 +21,28 @@ const themeReducer = (state: ThemeType, action: ThemeActionType) => {
       return { ...state, distance: action.payload };
     case ThemeActionsTypes.SET_RADIUS:
       return { ...state, radius: action.payload };
+    case ThemeActionsTypes.SET_POSITION_X:
+      return { ...state, positionX: action.payload };
+    case ThemeActionsTypes.SET_POSITION_X_OPPOSITE:
+      return { ...state, positionXOpposite: action.payload };
+    case ThemeActionsTypes.SET_POSITION_Y:
+      return { ...state, positionY: action.payload };
+    case ThemeActionsTypes.SET_POSITION_Y_OPPOSITE:
+      return { ...state, positionYOpposite: action.payload };
+    case ThemeActionsTypes.SET_ANGLE:
+      return { ...state, angle: action.payload };
+    case ThemeActionsTypes.SET_TEXT_COLOR:
+      return { ...state, textColor: action.payload };
+    case ThemeActionsTypes.SET_TEXT_COLOR_OPPOSITE:
+      return { ...state, textColorOpposite: action.payload };
+    case ThemeActionsTypes.SET_DARK_COLOR:
+      return { ...state, darkColor: action.payload };
+    case ThemeActionsTypes.SET_LIGHT_COLOR:
+      return { ...state, lightColor: action.payload };
+    case ThemeActionsTypes.SET_FIRST_GRADIENT_COLOR:
+      return { ...state, firstGradientColor: action.payload };
+    case ThemeActionsTypes.SET_SECOND_GRADIENT_COLOR:
+      return { ...state, secondGradientColor: action.payload };
     default:
       return state;
   }
@@ -34,6 +56,17 @@ const initialState: ThemeContextType = {
     distance: 20,
     activeLightSource: 1,
     radius: 50,
+    textColor: '#001f3f',
+    textColorOpposite: '#f6f5f7',
+    darkColor: '#dadada',
+    firstGradientColor: '#dadada',
+    lightColor: '#adadad',
+    secondGradientColor: '#adadad',
+    positionX: 30,
+    positionXOpposite: -30,
+    positionY: 30,
+    positionYOpposite: -30,
+    angle: 145,
   },
   dispatch: () => {},
 };
