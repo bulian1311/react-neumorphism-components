312ded6d5f4e400685f3d8a96b126835f58b9c2b
bulian1311 <bulian1311@gmail.com>
2021-12-28 12:15:03 +0300

    Theme types.

--- src/components/core/theme/theme.types.ts
+++ src/components/core/theme/theme.types.ts
@@ -1,3 +1,24 @@
+export type ThemeType = {
+  baseColor: string;
+  blur: number;
+  colorDifference: number;
+  distance: number;
+  activeLightSource: number;
+  radius: number;
+
+  positionX: number;
+  positionXOpposite: number;
+  positionY: number;
+  positionYOpposite: number;
+  angle: number;
+  textColor: string;
+  textColorOpposite: string;
+  darkColor: string;
+  lightColor: string;
+  firstGradientColor: string;
+  secondGradientColor: string;
+};
+
 export enum ThemeActionsTypes {
   SET_BASE_COLOR = 'SET_BASE_COLOR',
   SET_BLUR = 'SET_BLUR',
@@ -5,6 +26,17 @@ export enum ThemeActionsTypes {
   SET_DISTANCE = 'SET_DISTANCE',
   SET_ACTIVE_LIGHT_SOURCE = 'SET_ACTIVE_LIGHT_SOURCE',
   SET_RADIUS = 'SET_RADIUS',
+  SET_POSITION_X = 'SET_POSITION_X',
+  SET_POSITION_X_OPPOSITE = 'SET_POSITION_X_OPPOSITE',
+  SET_POSITION_Y = 'SET_POSITION_Y',
+  SET_POSITION_Y_OPPOSITE = 'SET_POSITION_Y_OPPOSITE',
+  SET_ANGLE = 'SET_ANGLE',
+  SET_TEXT_COLOR = 'SET_TEXT_COLOR',
+  SET_TEXT_COLOR_OPPOSITE = 'SET_TEXT_COLOR_OPPOSITE',
+  SET_DARK_COLOR = 'SET_DARK_COLOR',
+  SET_LIGHT_COLOR = 'SET_LIGHT_COLOR',
+  SET_FIRST_GRADIENT_COLOR = 'SET_FIRST_GRADIENT_COLOR',
+  SET_SECOND_GRADIENT_COLOR = 'SET_SECOND_GRADIENT_COLOR',
 }
 
 export type ThemeActionType =
@@ -31,16 +63,51 @@ export type ThemeActionType =
   | {
       type: ThemeActionsTypes.SET_RADIUS;
       payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_POSITION_X;
+      payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_POSITION_X_OPPOSITE;
+      payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_POSITION_Y;
+      payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_POSITION_Y_OPPOSITE;
+      payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_ANGLE;
+      payload: number;
+    }
+  | {
+      type: ThemeActionsTypes.SET_TEXT_COLOR;
+      payload: string;
+    }
+  | {
+      type: ThemeActionsTypes.SET_TEXT_COLOR_OPPOSITE;
+      payload: string;
+    }
+  | {
+      type: ThemeActionsTypes.SET_DARK_COLOR;
+      payload: string;
+    }
+  | {
+      type: ThemeActionsTypes.SET_LIGHT_COLOR;
+      payload: string;
+    }
+  | {
+      type: ThemeActionsTypes.SET_FIRST_GRADIENT_COLOR;
+      payload: string;
+    }
+  | {
+      type: ThemeActionsTypes.SET_SECOND_GRADIENT_COLOR;
+      payload: string;
     };
-
-export type ThemeType = {
-  baseColor: string;
-  blur: number;
-  colorDifference: number;
-  distance: number;
-  activeLightSource: number;
-  radius: number;
-};
 
 export type ThemeContextType = {
   state: ThemeType;
