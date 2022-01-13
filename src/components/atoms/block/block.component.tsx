39b5ff653a058832b6dfa9ac3e89d294431bfa39
bulian1311 <bulian1311@gmail.com>
2022-01-13 16:40:25 +0300

    Work on Block component.

--- src/components/atoms/block/block.component.tsx
+++ src/components/atoms/block/block.component.tsx
@@ -4,3 +4,9 @@ import { StyledBlock } from './block.styled';
 export const Block: React.FC = ({ children }) => {
   return <StyledBlock>{children}</StyledBlock>;
 };
+
+Block.displayName = 'Block';
+
+Block.propTypes = {};
+
+Block.defaultProps = {};
--- src/components/atoms/block/block.stories.tsx
+++ src/components/atoms/block/block.stories.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import { Story } from '@storybook/react';
 
 import { Block as BlockComponent } from './block.component';
-
-export default {
-  title: 'Atoms/Block',
-  component: BlockComponent,
-  argTypes: {
-    withoutBlock: {
-      type: { name: 'boolean', required: false },
-      name: 'Without Block',
-      control: { type: 'boolean' },
-    },
-  },
-} as Meta;
 
 const Template: Story = ({ withoutBlock }) => (
   <>
@@ -34,6 +22,15 @@ const Template: Story = ({ withoutBlock }) => (
 );
 
 export const Block = Template.bind({});
+
 Block.args = {
   withoutBlock: false,
+};
+
+Block.argTypes = {
+  withoutBlock: {
+    type: { name: 'boolean', required: false },
+    name: 'Without Block',
+    control: { type: 'boolean' },
+  },
 };
